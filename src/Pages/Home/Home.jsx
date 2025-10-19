ef3e7bd34a5046f554da1e886814a31ac6379c1a
tumpa <tumpadas3494@gmail.com>
2025-10-19 21:30:30 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "./Sections/HeroSection";
+
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <HeroSection />
+      
+    </div>
+  );
+};
+
+export default Home;
